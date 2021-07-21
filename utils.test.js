2ef06f92935e125b922ec3ede9a8c8d07af6fb1d
Elise Muellerleile <eliamue@live.com>
2021-07-21 11:29:23 -0500

    map test written

---
+++ utils.test.js
@@ -0,0 +1,12 @@
+import mapArray from './utils';
+
+describe('map array method', () => {
+  it('maps array with callback to multiply each number in the array by 2', async () => {
+    const callback = item => item * 2;
+    const array = [1, 3, 5, 7];
+    const expected = [2, 6, 10, 14];
+    const actual = mapArray(array, callback);
+
+    expect(expected).toEqual(actual);
+  });
+});
